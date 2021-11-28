34fce95e3a81e2e94bbafc5dc690ed7ef05f99e4
Wanlingj <Wanlingj@users.noreply.github.com>
2021-11-28 14:43:54 +0800

    Create 数组中重复的数字.cpp

---
+++ Sort/数组中重复的数字.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * 
+     * @param numbers int整型vector 
+     * @return int整型
+     */
+    int duplicate(vector<int>& numbers) {
+        // write code here
+        unordered_map<int,int> m;
+        for(int i=0;i<numbers.size();i++){
+            if (m[numbers[i]]>0)
+                return numbers[i];
+            m[numbers[i]]++;
+        }
+        return -1;
+    }
+};
