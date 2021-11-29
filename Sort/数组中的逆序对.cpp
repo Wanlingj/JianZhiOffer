6e9c91609dc12567aa1086d7f84e1a16d87984be
Wanlingj <Wanlingj@users.noreply.github.com>
2021-11-29 20:41:43 +0800

    Create 数组中的逆序对.cpp

---
+++ Sort/数组中的逆序对.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int mod = 1000000007;
+    int mergeSort(int left, int right, vector<int>& data, vector<int>& temp){
+        if (left >= right)    // 停止划分
+            return 0;
+        int mid = (left + right) / 2; //取中间
+        int res = mergeSort(left, mid, data, temp) + mergeSort(mid + 1, right, data, temp); //左右划分
+        res %= mod;  //防止溢出
+        int i = left, j = mid + 1;
+        for (int k = left; k <= right; k++)
+            temp[k] = data[k];
+        for (int k = left; k <= right; k++) {
+            if (i == mid + 1)
+                data[k] = temp[j++];
+            else if (j == right + 1 || temp[i] <= temp[j])
+                data[k] = temp[i++];
+            else { //左边比右边大，答案增加
+                data[k] = temp[j++];
+                res += mid - i + 1; // 统计逆序对
+            }
+        }
+        return res % mod;
+    }
+    int InversePairs(vector<int> data) {
+        int n = data.size();
+        vector<int> res(n);
+        return mergeSort(0, n - 1, data, res);
+    }
+};
